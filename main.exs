480e3ca7772900ca810d4fd7d11a85b0e5d9e1d7
alinakravchenko <101828799+alinakravchenko@users.noreply.github.com>
2023-11-20 18:09:33 +0000

    11.20.2023

--- main.exs
+++ main.exs
@@ -43,15 +43,53 @@ end
 
 def if_work(color) do
 if color == "blue" do
-"Yes"
-end
+"Yes" end
 end
 
 def if_short(color) do
 if color == "blue", do: "Yes" end
 
 def unless_work(color) do
-unless color == "red" do "Yes" end
+unless color == "red" do "Yes"
+else "No" end
 end
+
+def if_short2(color) do
+if color == "red", do: "Yes", else: "No" 
+end
+
+def case_nod(a,b) do   #8 6  |  6 - 2
+case rem(a,b) do   # 2      | 0
+0 -> b
+c -> case_nod(b,c) end  #case_nod(6,2)
+end
+
+def user({:user, age, name}) do
+ value_if = if age >= 18, do: "Welcome #{name}", else: "Go away #{name}"
+ value_if
+end
+
+def user_case(user) do
+case user do {:user, age, name} when age>=18 -> "Welcome #{name}"
+#<age verification>
+{:user, _, name}                             -> "Go away #{name}" 
+_                                            -> "Arror verity" end
+end
+
+def admin(user) do
+case user do {:user, _, _, :admin} -> :ok
+{:user, _, _, :moderator} -> :ok
+{:user, age, name, role} when age>=18        -> "Welcome #{name}"
+
+{:user, _, name}                             -> "Go away #{name}" 
+_                                            -> "Arror verity" end
+end
+
+def colors(current_color, figure) do
+case {current_color, figure} do
+{current_color, {color,_}} when (current_color == color) and (type == :pawn or type == :rock ) -> "Yes"
+_ -> "No" end
+end
+
 
 end
